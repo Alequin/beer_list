3082a91dfe66dc697f35339af5bd0a903419425c
James Cox <j-cox03@hotmail.com>
2017-08-02 19:15:33 +0100

    Made beer name as a large title

--- public/index.js
+++ public/index.js
@@ -34,8 +34,11 @@ function presentBeers(beers){
 function makeBeerElement(beer){
   var beerItem = document.createElement("ul");
 
-  var listElementsText = getBeerDetails(beer);
+  var title = document.createElement("h2");
+  title.innerText = beer.name;
+  beerItem.appendChild(title);
 
+  var listElementsText = getBeerDetails(beer);
   for(var text of listElementsText){
     var element = makeListElement(text);
     beerItem.appendChild(element);
@@ -46,7 +49,6 @@ function makeBeerElement(beer){
 
 function getBeerDetails(beer){
   var details = [];
-  details.push("Name: " + beer.name);
   details.push("Tag Line: " + beer.tagline);
   details.push("Description: " + beer.description);
   details.push("Tips: " + beer.brewers_tips);
