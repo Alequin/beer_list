a55cd51962db2a88cebb902ca4beb2e24d2ada98
James Cox <j-cox03@hotmail.com>
2017-08-02 18:44:26 +0100

    made request to api

--- public/index.js
+++ public/index.js
@@ -1,5 +1,27 @@
-var app = function () {
+var beerArray = [];
 
+function makeRequest(url, callback){
+  var request = new XMLHttpRequest();
+
+  request.addEventListener("load", callback);
+
+  request.open("GET", url);
+  request.send();
 }
+
+var app = function () {
+  makeRequest("https://api.punkapi.com/v2/beers", function(){
+    if(this.status !== 200){
+      return;
+    }
+
+    var jsonString = this.responseText;
+    beerArray = JSON.parse(jsonString);
+
+    console.log(beerArray);
+  });
+}
+
+
 
 window.addEventListener('load', app);
