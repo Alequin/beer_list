30b548b2e070c3f128fae9e664c2ea4d31e2e3a8
James Cox <j-cox03@hotmail.com>
2017-08-02 19:07:50 +0100

    Made methods to present details of each beer to screen

--- public/index.js
+++ public/index.js
@@ -17,7 +17,40 @@ function onRequest(){
   var jsonString = this.responseText;
   beerArray = JSON.parse(jsonString);
 
-  console.log(beerArray);
+  presentBeers(beerArray);
+}
+
+function presentBeers(beers){
+
+  var body = document.querySelector("body");
+
+  for(var beer of beers){
+    var beerElement = makeBeerElement(beer);
+    body.appendChild(beerElement);
+  }
+}
+
+function makeBeerElement(beer){
+  var beerItem = document.createElement("ul");
+
+  var listElementsText = [];
+  listElementsText.push("Name: " + beer.name);
+  listElementsText.push("Tag Line: " + beer.tagline);
+  listElementsText.push("Description: " + beer.description);
+  listElementsText.push("Tips: " + beer.brewers_tips);
+
+  for(var text of listElementsText){
+    var element = makeListElement(text);
+    beerItem.appendChild(element);
+  }
+
+  return beerItem;
+}
+
+function makeListElement(text){
+  var element = document.createElement("li");
+  element.innerText = text;
+  return element;
 }
 
 var app = function () {
